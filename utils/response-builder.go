8cd3ad72303d197696728732dd716b1fd2007f5d
varma1506 <durganarayanavarma@gmail.com>
2023-04-13 16:24:46 -0400

    Response structure added

--- utils/response-builder.go
+++ utils/response-builder.go
@@ -1,7 +1,18 @@
 package utils
 
-import "fmt"
+import (
+	"encoding/json"
+	models "main/BusService/models"
+	"net/http"
+)
 
-func main() {
-	fmt.Println("Hello")
+// Build a Response
+func BuildResponse(w http.ResponseWriter, code int, message string, data interface{}) {
+	var buildRequestResponse models.Response
+	buildRequestResponse.Message = message
+	buildRequestResponse.Data = data
+
+	//write response
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(buildRequestResponse)
 }
